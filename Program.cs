1a9852ac153285379935630f708ee23f48889d55
basrioglumehmet <basrioglumehmet@gmail.com>
2024-06-09 23:57:25 +0300

    educational purposes

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace try_catch_patika_dev
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int userIo = int.Parse(Console.ReadLine());
+                int userIo2 = int.Parse(Console.ReadLine());
+                int result = userIo / userIo2;
+                Console.WriteLine("Result:" + result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
